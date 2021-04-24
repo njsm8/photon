f6c1023fc6d085691041b6a0c27a5694df20cc86
Nischit Shetty <njmshetty@gmail.com>
2021-04-24 22:45:37 +0530

    final touches

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Photon</h1>
+      <h2>Stock Photos</h2>
+      <p>Best photos</p>
     </div>
   );
 };
